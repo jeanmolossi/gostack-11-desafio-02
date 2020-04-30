ea8c34c73a893b2b709b9e5927e8dabfe6d70872
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2020-04-30 14:12:13 -0300

    Aplicação atualizada para envio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(newRepo);
+
+  return response.json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepo = repositories.findIndex( singleRepo => singleRepo.id === id );
+  if( findRepo < 0 ){
+    return response.status(400).json({error: 'Repository identified not found!'});
+  }
+  const { title, url, techs } = request.body;
+
+  repositories[findRepo] = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepo].likes
+  }
+
+  return response.json(repositories[findRepo]);
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepo = repositories.findIndex( singleRepo => singleRepo.id === id );
+  if( findRepo < 0 ){
+    return response.status(400).json({error: 'Repository identified not found!'});
+  }
+  repositories.splice(findRepo, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const findRepo = repositories.findIndex( singleRepo => singleRepo.id === id );
+  if( findRepo < 0 ){
+    return response.status(400).json({error: 'Repository identified not found!'});
+  }
+
+  const repoSelected = repositories[findRepo];
+
+  repositories[findRepo] = {
+    ...repoSelected,
+    likes: repoSelected.likes + 1
+  }
+
+  return response.json(repositories[findRepo]);
+
 });
 
 module.exports = app;
